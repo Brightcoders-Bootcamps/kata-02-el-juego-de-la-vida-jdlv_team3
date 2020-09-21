a8f3d4bb9d45b10d1902a3dd1f63f083caef3008
oswaldotovar32 <52464740+oswaldotovar32@users.noreply.github.com>
2020-09-21 13:06:50 -0500

    add object class
    Co-Authored-By: alcamk <58040748+alcamk@users.noreply.github.com>

--- app.js
+++ app.js
@@ -1,3 +1,83 @@
+class cell {
+
+    constructor(life, neighbors) {
+        this.life = life;
+        this.neighbors = neighbors;
+    }
+
+    // CONDICIONES PARA LA CELULA
+    // 1 = VIVO    0 = MUERTO
+
+    getLife() {
+        return this.life;
+    }
+
+    setLife(life) {
+        this.life = life;
+    }
+
+    setNeighbors(Neighbors) {
+        this.neighbors = Neighbors;
+    }
+
+    getNumCell() {
+        if (this.life == 0) {
+            return '.';
+        } else {
+            return '*';
+        }
+    }
+
+    getNeighbors() {
+        return this.neighbors;
+    }
+
+
+};
+
+let numX = 4, numY = 8;
+var resutArrayCell = '';
+var resutNewArray = '';
+var cell;
+let arrayCells;
+let loopControler=0;
+
+function initGame(){
+    //let arrayCells;
+    resutArrayCell = '';
+    //numX = parseInt(prompt('Ingrese el numero de filas: '));
+    //numY = parseInt(prompt('Ingrese el numero de columnas: '));
+    arrayCells = new Array(numX);
+    let i = 0;
+    while(i < arrayCells.length){
+
+    }
+    for (i = 0; i < arrayCells.length; i++) {
+        arrayCells[i] = new Array(numY);
+    }
+
+    for (let x = 0; x < numX; x++) {
+        for (let y = 0; y < numY; y++) {
+            var randomvar = Math.random();
+            if (randomvar >= 0.5) {
+                cell = new celula(0, 0);
+                arrayCells[x][y] = cell;
+            } else {
+                cell = new celula(1, 0);
+                arrayCells[x][y] = cell;
+            }
+            resutArrayCell+= arrayCells[x][y].getNumCell();
+        }
+        resutArrayCell += '\n';
+    }
+    newArray=arrayCells;
+    //console.log(resutArrayCell);
+    //iteration(arrayCells);
+    return resutArrayCell;
+}
+
+
+
 function sum(a, b) {
     return a + b;
 }
--- app.test.js
+++ app.test.js
@@ -1,5 +1,19 @@
-const sum = require('./app');
+const game = require('./game');
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(3);
+test('La suma es correcta', () => {
+    expect(game.sumTd(1, 2)).toBe(3);
+});
+
+test('Las filas es igual a 4', () => {
+    expect(game.getNumX()).toBe(4);
+});
+
+test('Las columnas es igual a 8', () => {
+    expect(game.getNumY()).toBe(8);
+});
+
+test('Se completo el juego correctamente', () => {
+    let arrayCell = game.getArrayCell();
+    let newArray = game.getNewArray();
+    expect(arrayCell).toBe(newArray);
 });
