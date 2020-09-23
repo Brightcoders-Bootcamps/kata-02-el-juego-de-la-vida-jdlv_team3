725474c2434c5aa578cc670e37aaa4b73d1369de
oswaldotovar32 <52464740+oswaldotovar32@users.noreply.github.com>
2020-09-23 12:39:50 -0500

    add rules and solve problems with the CodeClimate
    Co-Authored-By: alcamk <58040748+alcamk@users.noreply.github.com>

--- app.js
+++ app.js
@@ -2,10 +2,8 @@
 class Celula {
 
     constructor (life, neighbors) {
-
         this.life = life;
         this.neighbors = neighbors;
-
     }
 
     /*
@@ -14,55 +12,35 @@ class Celula {
      */
 
     getLife () {
-
         return this.life;
-
     }
 
     setLife (life) {
-
         this.life = life;
-
     }
 
     setNeighbors (Neighbors) {
-
         this.neighbors = Neighbors;
-
     }
 
     getNumCell () {
-
         if (this.life === 0) {
-
             return ".";
-
         }
-
         return "*";
-
-
     }
 
     getNeighbors () {
-
         return this.neighbors;
-
     }
 
 }
 
 function getCell (cell) {
-
     if (cell === 0) {
-
         return ".";
-
     }
-
     return "*";
-
-
 }
 
 const numX = 4,
@@ -75,67 +53,42 @@ let arrayCells = "",
 const getNumX = () => numX;
 
 function getNumY () {
-
     return numY;
-
 }
 
 function getArrayCell () {
-
     return resutArrayCell;
-
 }
 
 function getNewArray () {
-
     return resutArrayCell;
-
 }
 
 play();
 
-function numCell(){
-    
-}
-
 function play () {
-
     const resultGame = initGame();
-
     console.log(resultGame);
     iteration(arrayCells);
     const showRes = showResult(newArray);
-
     console.log(showRes);
-
 }
 
 function initGame () {
-
-    // Let arrayCells;
     resutArrayCell = "";
-
     arrayCells = new Array(numX);
     for (i = 0; i < arrayCells.length; i++) {
-
         arrayCells[i] = new Array(numY);
-
     }
-
     for (let x = 0; x < numX; x++) {
-
         for (let y = 0; y < numY; y++) {
-
             const randomvar = Math.random();
-
             if (randomvar >= 0.5) {
-
                 cell = new Celula(
                     0,
                     0
                 );
                 arrayCells[x][y] = cell;
-
             } else {
 
                 cell = new Celula(
@@ -143,132 +96,77 @@ function initGame () {
                     0
                 );
                 arrayCells[x][y] = cell;
-
             }
             resutArrayCell += arrayCells[x][y].getNumCell();
-
         }
         resutArrayCell += "\n";
-
     }
-    newArray = arrayCells;
-
-    /*
-     * Console.log(resutArrayCell);
-     * iteration(arrayCells);
-     */
+    newArray = arrayCells;    
+    console.log(resutArrayCell);
+    iteration(arrayCells);
     return resutArrayCell;
-
 }
 
 function iteration (ParamarrayCells) {
-
     for (let x = 0; x < ParamarrayCells.length; x++) {
-
         for (let y = 0; y < ParamarrayCells[x].length; y++) {
-
             let neighbors = 0;
-
             for (let i = -1; i <= 1; i++) {
-
                 for (let j = -1; j <= 1; j++) {
-
                     try {
-
                         if (ParamarrayCells[x + i][y + j].getLife() == 1) {
-
                             if (ParamarrayCells[x][y].getLife() == 1 && i == 0 && j == 0) {
-                                // Console.log('se conto solo');
                             } else {
-
                                 neighbors++;
-
                             }
-
                         }
-
                     } catch (e) {
-
                     }
-
                 }
-
             }
-            //console.log(`Vivo/muerto: ${ParamarrayCells[x][y].getLife()}. Vecino vivos: ${neighbors}. Fila: ${x}. Columna: ${y}`);
             ParamarrayCells[x][y].setNeighbors(neighbors);
             rulesLife(
                 ParamarrayCells[x][y],
                 x,
                 y
             );
-
         }
-
     }
-
 }
 
 function rulesLife (cell, x, y) {
-
-    // Console.log("vecinos=" + neighbors + "   fila" + x + "  columna=" + y)
     if (cell.getLife() == 1 && cell.getNeighbors() < 2) {
-
         newArray[x][y].setLife(0); // Soledad
-
     } else if (cell.getLife() == 1 && cell.getNeighbors() == 2 || cell.getNeighbors() == 3) {
-
         newArray[x][y].setLife(1);
-
     } else if (cell.getLife() == 1 && cell.getNeighbors() > 3) {
-
         newArray[x][y].setLife(0); // Sobrepoblación
-
     } else if (cell.getLife() == 0 && cell.getNeighbors() == 3) {
-
         newArray[x][y].setLife(1); // Reproducción
-
     }
-
 }
 
 function rulesLifes(cell, neighbors){
     if (cell == 1 && neighbors < 2) {
-
         return 0; // Soledad
-
     } else if (cell == 1 && neighbors == 2 || neighbors == 3) {
-
         return 1;
-
     } else if (cell == 1 && neighbors > 3) {
-
         return 0; // Sobrepoblación
-
     } else if (cell == 0 && neighbors == 3) {
-
         return 1; // Reproducción
-
     }
-
 }
 
 function showResult (array) {
-
     resutNewArray = "";
     for (let x = 0; x < array.length; x++) {
-
         for (let y = 0; y < array[x].length; y++) {
-
             resutNewArray += arrayCells[x][y].getNumCell();
-
         }
         resutNewArray += "\n";
-
     }
-
     return resutNewArray;
-    // Console.log(resutNewArray);
-
 }
 
 
--- app.test.js
+++ app.test.js
@@ -32,6 +32,17 @@ describe('Life Games', () => {
             expect(game.rulesLifes(1, 4)).toBe(0);
         });
 
+        test("Any live cell with two or three live neighbours lives on to the next generation.", () => {
+
+            expect(game.rulesLifes(1, 2)).toBe(1);
+            expect(game.rulesLifes(1, 3)).toBe(1);
+        });
+
+        test("Any dead cell with exactly three live neighbours becomes a live cell.", () => {
+
+            expect(game.rulesLifes(0, 3)).toBe(1);
+        });
+
     });
 });
 
